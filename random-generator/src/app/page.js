236b8b102f9d54eba53d51e5dc24b18ab94cd441
Kenny <kenny-maeder@web.de>
2023-08-12 17:42:40 +0200

    second approach to the function (still does not work)

--- random-generator/src/app/page.js
+++ random-generator/src/app/page.js
@@ -1,14 +1,18 @@
-import Image from 'next/image'
+import { useState } from 'react';
 import words from './components/words.json'
 import RandomGenerateButton from './components/randomGenerateButton';
 
 export default function Home() {
+  const handleClick = () => {
+    alert('Knopf gedrückt!');
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <header>
         <h1 className="underline underline-offset-3 mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Random Generator for Drawing</h1>
       </header>
-      <RandomGenerateButton />
+      <RandomGenerateButton onClick={handleClick}/>
       <div className="container mx-auto">
         <div className="flex justify-between">
           <div className="w-1/2 text-center">
